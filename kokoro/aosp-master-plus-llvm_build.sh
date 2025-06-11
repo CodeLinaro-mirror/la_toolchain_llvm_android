a53d11c1e57e00cc16d457bb5572956f8d380c50
Yi Kong <yikong@google.com>
2025-06-11 21:57:00 +0900

    kokoro: Fix aosp-master-plus-llvm build
    The supported target release is now `aosp_current`.
    
    Test: N/A
    Change-Id: Ia18e0bea2d288466b3f0d3232376e9909ac5a827

--- kokoro/aosp-master-plus-llvm_build.sh
+++ kokoro/aosp-master-plus-llvm_build.sh
@@ -26,7 +26,7 @@ DIST_DIR=dist \
 OUT_DIR=out \
 prebuilts/python/linux-x86/bin/python3 \
   toolchain/llvm_android/test_compiler.py --build-only \
-  --target ${AOSP_BUILD_TARGET}-trunk_staging-userdebug \
+  --target ${AOSP_BUILD_TARGET}-aosp_current-userdebug \
   --module sync \
   --clang-package-path ${KOKORO_GFILE_DIR} .
 
