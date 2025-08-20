98b19e4b4f027fa6c05cd0f754d77d317aa75b7c
Pirama Arumuga Nainar <pirama@google.com>
2025-08-20 11:45:52 -0700

    Remove _LIBCPP_HAS_MUSL_LIBC cpp define when building musl
    Bug: http://b/379357589
    
    This should now be set by c++/v1/__config_site.
    
    Test: build locally with `--musl`
    Change-Id: Id0916b0c3bad953b52d19f06c683ec6a7587e4cc

--- src/llvm_android/configs.py
+++ src/llvm_android/configs.py
@@ -347,11 +347,6 @@ class LinuxMuslConfig(LinuxConfig):
     def cflags(self) -> List[str]:
         cflags = super().cflags + [
                 f'--target={self.llvm_triple}',
-                # TODO: Try removing -D_LIBCPP_HAS_MUSL_LIBC now that the
-                # LIBCXX_HAS_MUSL_LIBC CMake var is set. We may have to wait
-                # until the bootstrap compiler's c++/v1/__config_site header is
-                # new enough to define _LIBCPP_HAS_MUSL_LIBC to 1.
-                '-D_LIBCPP_HAS_MUSL_LIBC',
                 '-D_LARGEFILE64_SOURCE=1',
                 # gcc does this automatically and glibc includes it in features.h
                 # Neither clang nor musl include it, so add it here.  Otherwise
