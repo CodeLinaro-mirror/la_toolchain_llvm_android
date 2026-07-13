67669916d9e4f6058a20e9a60ed17f9477e50d7f
Sharjeel Khan <sharjeelkhan@google.com>
2026-07-13 18:43:06 +0000

    Update LLVM version constants to r614150
    Update version constants for the LLVM r614150 update.
    
    TAG=agy
    CONV=489cbcab-199d-4f52-a9c6-c2a34f33ba42
    
    Bug: 531824765
    Flag: NONE compiler update
    Test: local build
    Change-Id: I03cbb7243a86473b5cec2625d35b083df9e79dfd

--- src/llvm_android/android_version.py
+++ src/llvm_android/android_version.py
@@ -19,9 +19,9 @@ import re
 _llvm_next = False
 _version_read = False
 
-_patch_level = '2'
-_svn_revision = 'r596125'
-_git_sha = '2bc22ea02edda5926f3e53f141def9bf212ac1db'
+_patch_level = '1'
+_svn_revision = 'r614150'
+_git_sha = '2cf353b5e8560723409f3f9164bddec76f499963'
 
 # Psudo revision for top of trunk LLVM.
 _svn_revision_next = 'r99999999'
