a2434e686a4de26641cb90cf2f05b2ef5e793cfe
Group Agent Android-llvm <groupagent-android-llvm@google.com>
2026-08-05 18:28:05 -0700

    Update llvm build for jemalloc in musl sysroot
    The musl sysroot now links against libjemalloc5.so by default for better performance.  Copy libjemalloc5.so into the output artifacts. Also copy the notice file.
    
    TAG=agy
    CONV=52bf3bd3-ccd0-4e37-83db-14a82d6601e3
    
    Bug: 533100825
    Test: ABTD
    Flag: EXEMPT BUGFIX
    Change-Id: If3fd724be7d69385713c771ebfc7580126809cd4

--- src/llvm_android/base_builders.py
+++ src/llvm_android/base_builders.py
@@ -726,6 +726,7 @@ class LLVMBuilder(LLVMBaseBuilder):
         if isinstance(self._config, configs.LinuxMuslConfig):
             shutil.copy2(self._config.sysroot / 'lib' / 'libc_musl.so', lib_dir / 'libc_musl.so')
             shutil.copy2(self._config.sysroot / 'lib' / 'libjemalloc5.so', lib_dir / 'libjemalloc5.so')
+            shutil.copy2(self._config.sysroot / 'LICENSE', lib_dir / 'LICENSE.musl')
 
     def _setup_install_dir(self) -> None:
         if self.swig_executable:
